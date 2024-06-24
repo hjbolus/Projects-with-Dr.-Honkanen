7b8b0180b2f79520fa5575677faae574a8f91607
hjbolus <141349319+hjbolus@users.noreply.github.com>
2024-06-24 15:27:06 -0500

    Update Amino acid motif search.py

--- Amino acid motif search.py
+++ Amino acid motif search.py
@@ -54,6 +54,6 @@ for protein in proteins:
     else:
         print(response.status_code)
 
-new_df = pd.DataFrame(found_motifs, columns=[uniprot_column,'motif'])
+new_df = pd.DataFrame(found_motifs.items(), columns=[uniprot_column,'SLiM'])
 df.merge(new_df, how='left', on=uniprot_column).to_excel('/'.join(file.split('/')[0:-1]) + file.split('/')[-1].split('.')[0]+'motif search.xlsx')
 
