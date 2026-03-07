be546ddfef71d17272d75fb9e6f944be7b9db3cf
hjbolus <141349319+hjbolus@users.noreply.github.com>
2026-03-07 16:26:57 -0600

    Update GSEA for phosphoproteomics.r

--- GSEA pipeline/GSEA for phosphoproteomics.r
+++ GSEA pipeline/GSEA for phosphoproteomics.r
@@ -15,27 +15,27 @@ if (!exists("phos_results_table", envir=globalenv())) {
 }
 
 # calculate ranks
-c(phos_results_table, phos_ranks_msd) %<-% calc_ranks(
+c(phos_results_table, phos_ranks_pi_stat) %<-% calc_ranks(
   df = phos_results_table,
   id_col = 'gene_id',
   logfc_col = 'logFC', 
   p_col = 'PValue',
-  rank_type = 'msd',
+  rank_type = 'pi_stat',
   abs = TRUE
 )
 
 # plot ranks against log2FC and -log10(p-value). Available metrics are "signed_p", "pi_stat", and "msd".
-save_gsea_plot(plot_metric(phos_results_table, "signed_p"), paste0(output_dir, "phos_ranks_signed_p.png"))
+save_gsea_plot(plot_metric(phos_results_table, "pi_stat"), paste0(output_dir, "phos_ranks_pi_stat.png"))
 
 # run fGSEA. scoreType = "pos" is important for phosphoproteomics, where enrichment/depletion does not directly map onto activity
-phos_signed_p_h <- run_and_plot_fgsea(h_gs_gsymbol, phos_ranks_signed_p, scoreType="pos")
+phos_signed_p_h <- run_and_plot_fgsea(h_gs_gsymbol, phos_ranks_pi_stat, scoreType="pos")
 
 # save figures
-save_sig_enrichment_plots(results = phos_signed_p_h,
+save_sig_enrichment_plots(results = phos_pi_stat_h,
                           genesets = h_gs_gsymbol,
-                          ranks = phos_ranks_signed_p,
+                          ranks = phos_ranks_pi_stat,
                           path = output_dir,
-                          name = "phos_signed_p_h")
+                          name = "phos_pi_stat_h")
 
 # save complete results
-save(phos_signed_p_h, file=paste0(output_dir, "phos_signed_p_h", ".RData"))
+save(phos_signed_p_h, file=paste0(output_dir, "phos_pi_stat_h", ".RData"))
